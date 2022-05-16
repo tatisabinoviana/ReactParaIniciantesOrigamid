67dc676a1d0dad2ab1853046b6a285508e3745a9
tatisabinoviana <tatisabinoviana@gmail.com>
2022-05-16 09:52:16 -0300

    demonstracao do primeiro exemplo sobre arrays dependentes no userEffect

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
 import React from 'react';
 
-import Produtos from './Produtos';
-
 const App = () => {
-  const [dados, setDados] = React.useState(null);
-  const [carregando, setCarregando] = React.useState(null);
+  const [contar, setContar] = React.useState(0);
 
-  async function handleClick(event) {
-    setCarregando(true);
-    const response = await fetch(
-      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
-    );
-    const json = await response.json();
-    setDados(json);
-    setCarregando(false);
-  }
-  return (
-    <div>
-      <button style={{ margin: '.5rem' }} onClick={handleClick}>
-        notebook
-      </button>
-      <button style={{ margin: '.5rem' }} onClick={handleClick}>
-        smartphone
-      </button>
-      <button style={{ margin: '.5rem' }} onClick={handleClick}>
-        tablet
-      </button>
-      {carregando && <p>Carregando...</p>}
-      {!carregando && dados && <Produtos dados={dados} />}
-    </div>
-  );
+  React.useEffect(() => {
+    console.log('Executou');
+  }, []);
+  React.useEffect(() => {
+    document.title = 'Total ' + contar;
+  }, [contar]);
+  return <button onClick={() => setContar(contar + 1)}>{contar}</button>;
 };
 
 export default App;
