ec47dd7aa99291a59ee434ec4e52b1fe04c02d9f
tatisabinoviana <tatisabinoviana@gmail.com>
2022-05-16 10:48:56 -0300

    exemplo 2 - usando fetch para trazer os dados dentro do useEfect

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from 'react';
 
 const App = () => {
   const [contar, setContar] = React.useState(0);
+  const [dados, setDados] = React.useState(null);
 
   React.useEffect(() => {
-    console.log('Executou');
+    fetch('https://ranekapi.origamid.dev/json/api/produto/notebook')
+      .then((response) => response.json())
+      .then((json) => setDados(json));
+    console.log('oi');
   }, []);
-  React.useEffect(() => {
-    document.title = 'Total ' + contar;
-  }, [contar]);
-  return <button onClick={() => setContar(contar + 1)}>{contar}</button>;
+  return (
+    <div>
+      {dados && (
+        <div>
+          <h1>{dados.nome}</h1>
+          <p>R$ {dados.preco * contar}</p>
+        </div>
+      )}
+      <button onClick={() => setContar(contar + 1)}>{contar}</button>
+    </div>
+  );
 };
 
 export default App;
