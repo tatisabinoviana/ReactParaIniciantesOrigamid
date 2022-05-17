3646feeb9d2efbc21f370a3245654348bdf574f0
tatisabinoviana <tatisabinoviana@gmail.com>
2022-05-17 15:40:31 -0300

    base inicial para aprender useRef

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import React from 'react';
-import Produto from './Produto';
 
 const App = () => {
-  const [produto, setProduto] = React.useState(null);
+  const video = React.useRef();
 
   React.useEffect(() => {
-    const produtoLocal = window.localStorage.getItem('produto');
-    if (produtoLocal !== null) setProduto(produtoLocal);
-  }, []);
+    video.current = 'Teste';
+    console.log(video.current);
+  });
 
-  React.useEffect(() => {
-    if (produto !== null) window.localStorage.setItem('produto', produto);
-  }, [produto]);
-
-  function handleClick({ target }) {
-    setProduto(target.innerText);
-  }
-  return (
-    <div>
-      <h1>Preferência: {produto}</h1>
-      <button onClick={handleClick} style={{ marginRight: '1rem' }}>
-        notebook
-      </button>
-      <button onClick={handleClick} style={{ marginRight: '1rem' }}>
-        smartphone
-      </button>
-      <Produto produto={produto} />
-    </div>
-  );
+  return <video ref={video} />;
 };
 
 export default App;
