789f5a2aaa5068e4b849685c7db8edbf6b7266b4
tatisabinoviana <tatisabinoviana@gmail.com>
2022-05-20 15:49:52 -0300

    ao utilizar oum hook dentro de uma funcao, deve-se usar a nomenclatura da funcao como useNomeDaFuncao

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
 import React from 'react';
-import useLocalStorage from './useLocalStorage';
-import useFetch from './useFetch';
+
+function useNumeroAleatorio() {
+  React.useEffect(() => {
+    document.title = Math.random();
+  }, []);
+}
 
 const App = () => {
-  const [produto, setProduto] = useLocalStorage('produto', '');
-  const { request, data, loading, error } = useFetch();
-
-  React.useEffect(() => {
-    async function fetchData() {
-      const { response, json } = await request(
-        'https://ranekapi.origamid.dev/json/api/produto/',
-      );
-    }
-    fetchData();
-  }, [request]);
-
-  function handleClick({ target }) {
-    setProduto(target.innerText);
-  }
-
-  if (error) return <p>{error}</p>;
-  if (loading) return <p>Carregando...</p>;
-  if (data)
-    return (
-      <div>
-        <p>Produto preferido: {produto}</p>
-        <button onClick={handleClick}>notebook</button>
-        <button onClick={handleClick}>smartphone</button>
-
-        {data.map((produto) => (
-          <div key={produto.id}>
-            <h1>{produto.nome}</h1>
-          </div>
-        ))}
-      </div>
-    );
-  else return null;
+  useNumeroAleatorio();
+  return <div></div>;
 };
 
 export default App;
