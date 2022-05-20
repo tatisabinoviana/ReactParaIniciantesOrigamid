9d7dbe0e53c6a16284d07097048a1d8522d133e7
tatisabinoviana <tatisabinoviana@gmail.com>
2022-05-20 10:04:53 -0300

    resolucao do desafio para apresentar os dados do link json e tem um botao para limpar os dados. esses dados sao apresentados usando useEffect

--- src/GlobalContext.js
+++ src/GlobalContext.js
@@ -3,16 +3,20 @@ import React from 'react';
 export const GlobalContext = React.createContext();
 
 export const GlobalStorage = ({ children }) => {
-  const [contar, setContar] = React.useState(0);
-  function adicionarUm() {
-    setContar((contar) => contar + 1);
-  }
-  function adicionarDois() {
-    setContar((contar) => contar + 2);
+  const [dados, setDados] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch(`https://ranekapi.origamid.dev/json/api/produto/`)
+      .then((response) => response.json())
+      .then((json) => setDados(json));
+  }, []);
+
+  function limparDados() {
+    setDados(null);
   }
 
   return (
-    <GlobalContext.Provider value={{ contar, adicionarUm, adicionarDois }}>
+    <GlobalContext.Provider value={{ dados, limparDados }}>
       {children}
     </GlobalContext.Provider>
   );
--- src/Produto.js
+++ src/Produto.js
@@ -3,11 +3,14 @@ import { GlobalContext } from './GlobalContext';
 
 const Produto = () => {
   const global = React.useContext(GlobalContext);
-
+  console.log(global);
+  if (global.dados === null) return null;
   return (
     <div>
-      Produto: {global.contar}{' '}
-      <button onClick={() => global.adicionarDois()}>Adicionar</button>
+      Produto:
+      {global.dados.map((produto) => (
+        <li key={produto.id}>{produto.nome}</li>
+      ))}
     </div>
   );
 };
