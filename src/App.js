61054dad83dbb8a4e044c67ef7a3404e44a204bc
tatisabinoviana <tatisabinoviana@gmail.com>
2022-05-23 16:08:46 -0300

    defionicoes do textarea no react

--- src/App.js
+++ src/App.js
@@ -1,62 +1,15 @@
 import React from 'react';
 
-const formFields = [
-  { id: 'nome', label: 'Nome', type: 'text' },
-  { id: 'email', label: 'Email', type: 'email' },
-  { id: 'senha', label: 'Senha', type: 'password' },
-  { id: 'cep', label: 'Cep', type: 'text' },
-  { id: 'rua', label: 'Rua', type: 'text' },
-  { id: 'numero', label: 'Número', type: 'text' },
-  { id: 'bairro', label: 'Bairro', type: 'text' },
-  { id: 'cidade', label: 'Cidade', type: 'text' },
-  { id: 'estado', label: 'Estado', type: 'text' },
-];
-
 const App = () => {
-  const [form, setForm] = React.useState(
-    formFields.reduce((acc, field) => {
-      return { ...acc, [field.id]: '' };
-    }, {}),
-  );
-
-  const [response, setResponse] = React.useState(null);
-
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    fetch('https://ranekapi.origamid.dev/json/api/usuario', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-
-      body: JSON.stringify(form),
-    }).then((response) => {
-      setResponse(response);
-    });
-  }
-
-  function handleChange({ target }) {
-    const { id, value } = target;
-    setForm({ ...form, [id]: value });
-  }
-
+  const [textarea, setTextarea] = React.useState('');
   return (
-    <form onSubmit={handleSubmit}>
-      {formFields.map(({ id, label, type }) => (
-        <div key={id}>
-          <label htmlFor={id}>{label}</label>
-          <input
-            id={id}
-            type={type}
-            name={label}
-            value={form[id]}
-            onChange={handleChange}
-          />
-        </div>
-      ))}
-      {response && response.ok && <p>Formulário Enviado</p>}
-      <button>Enviar</button>
+    <form>
+      <textarea
+        value={textarea}
+        onChange={({ target }) => setTextarea(target.value)}
+        rows="5"
+      ></textarea>
+      {textarea}
     </form>
   );
 };
