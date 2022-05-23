ecc4b2322db919d45c2f9fe5a95bace536a4199d
tatisabinoviana <tatisabinoviana@gmail.com>
2022-05-23 14:58:21 -0300

    resolucao do exercicio, fazendo um formulario de post

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React from 'react';
 
+const formFields = [
+  { id: 'nome', label: 'Nome', type: 'text' },
+  { id: 'email', label: 'Email', type: 'email' },
+  { id: 'senha', label: 'Senha', type: 'password' },
+  { id: 'cep', label: 'Cep', type: 'text' },
+  { id: 'rua', label: 'Rua', type: 'text' },
+  { id: 'numero', label: 'Número', type: 'text' },
+  { id: 'bairro', label: 'Bairro', type: 'text' },
+  { id: 'cidade', label: 'Cidade', type: 'text' },
+  { id: 'estado', label: 'Estado', type: 'text' },
+];
+
 const App = () => {
-  const [form, setForm] = React.useState({
-    nome: '',
-    email: '',
-  });
+  const [form, setForm] = React.useState(
+    formFields.reduce((acc, field) => {
+      return { ...acc, [field.id]: '' };
+    }, {}),
+  );
+
+  const [response, setResponse] = React.useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(event);
+
+    fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+
+      body: JSON.stringify(form),
+    }).then((response) => {
+      setResponse(response);
+    });
   }
 
   function handleChange({ target }) {
@@ -18,24 +43,19 @@ const App = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="nome">Nome</label>
-      <input
-        id="nome"
-        type="text"
-        name="nome"
-        value={form.nome}
-        onChange={handleChange}
-      />
-      {form.nome}
-      <label htmlFor="email">Email</label>
-      <input
-        id="email"
-        type="email"
-        name="email"
-        value={form.email}
-        onChange={handleChange}
-      />
-      {form.email}
+      {formFields.map(({ id, label, type }) => (
+        <div key={id}>
+          <label htmlFor={id}>{label}</label>
+          <input
+            id={id}
+            type={type}
+            name={label}
+            value={form[id]}
+            onChange={handleChange}
+          />
+        </div>
+      ))}
+      {response && response.ok && <p>Formulário Enviado</p>}
       <button>Enviar</button>
     </form>
   );
