15153d4edb47a65db058ab73cee00aecb6d8027e
tatisabinoviana <tatisabinoviana@gmail.com>
2022-05-24 08:57:20 -0300

    duas formas de configurar o checkbox

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import React from 'react';
 
 const App = () => {
-  const [select, setSelect] = React.useState('');
+  const [produto, setProduto] = React.useState('');
+  const [cor, setCor] = React.useState('');
+
+  function handleChange({ target }) {
+    setProduto(target.value);
+  }
   return (
     <form>
-      <select
-        value={select}
-        onChange={({ target }) => setSelect(target.value)}
-        id="produtos"
-      >
-        <option disabled value="">
-          Selecione
-        </option>
-        <option value="notebook">Notebook</option>
-        <option value="smartphone">Smartphone</option>
-        <option value="tablet">Tablet</option>
-      </select>
-      {select}
+      <h2>Cores</h2>
+      <label>
+        <input
+          type="radio"
+          onChange={({ target }) => setCor(target.value)}
+          name="cor"
+          value="azul"
+        />
+        Azul
+      </label>
+      <label>
+        <input
+          type="radio"
+          onChange={({ target }) => setCor(target.value)}
+          name="cor"
+          value="vermelho"
+        />
+        Vermelho
+      </label>
+      <h2>Produtos</h2>
+      <label>
+        <input
+          type="radio"
+          onChange={handleChange}
+          checked={produto === 'smartphone'}
+          value="smartphone"
+        />
+        Smartphone
+      </label>
+      <label>
+        <input
+          type="radio"
+          onChange={handleChange}
+          checked={produto === 'notebook'}
+          value="notebook"
+        />
+        Notebook
+      </label>
     </form>
   );
 };
