7daa9ac801e18d9ff8335be9d1a3a73dd4a90651
tatisabinoviana <tatisabinoviana@gmail.com>
2022-05-24 11:46:58 -0300

    checkbox separado por cores, onde desmarcar e marcar é reativo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from 'react';
 
 const App = () => {
-  const [termos, setTermos] = React.useState(false);
+  const [cores, setCores] = React.useState(['vermelho']);
+
+  function handleChange({ target }) {
+    if (target.checked) {
+      setCores([...cores, target.value]);
+    } else {
+      setCores(cores.filter((cor) => cor !== target.value));
+    }
+  }
+
   return (
     <form>
-      {termos && <p>Aceitou os termos.</p>}
       <label>
         <input
           type="checkbox"
-          value="Termos"
-          checked={termos}
-          onChange={({ target }) => setTermos(target.checked)}
+          value="azul"
+          checked={cores.includes('azul')}
+          onChange={handleChange}
         />
-        Aceito os termos.
+        Azul
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          value="vermelho"
+          checked={cores.includes('vermelho')}
+          onChange={handleChange}
+        />
+        Vermelho
       </label>
     </form>
   );
