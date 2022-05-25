97787daecd2caf21c111f8483ec58534488b508c
tatisabinoviana <tatisabinoviana@gmail.com>
2022-05-25 14:21:56 -0300

    producao do checkbox componetizado, onde cada caixa é selecionada e incluída numa array

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Checkbox from './Form/Checkbox';
 import Input from './Form/Input';
 import Radio from './Form/Radio';
 import Select from './Form/Select';
@@ -9,11 +10,23 @@ const App = () => {
   const [produto, setProduto] = React.useState('');
   const [cor, setCor] = React.useState('Vermelho');
   const [frutas, setFrutas] = React.useState('');
+  const [linguagens, setLinguagens] = React.useState([]);
 
   return (
     <form>
+      <h2>Checkbox</h2>
+      <Checkbox
+        options={['JavaScript', 'PHP', 'Ruby']}
+        value={linguagens}
+        setValue={setLinguagens}
+      />
+
       <h2>Cores</h2>
-      <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
+      <Radio
+        options={['Azul', 'Vermelho', 'Amarelo']}
+        value={cor}
+        setValue={setCor}
+      />
       <h2>Frutas</h2>
       <Radio
         options={['Limão', 'Laranja', 'Uva']}
