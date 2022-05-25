9dff8fa5d10764a102fdf6e0a2e602b9b77536fb
tatisabinoviana <tatisabinoviana@gmail.com>
2022-05-25 13:25:43 -0300

    configuracao e padronizacao do component de radio button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import Input from './Form/Input';
+import Radio from './Form/Radio';
 import Select from './Form/Select';
 
 const App = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [produto, setProduto] = React.useState('');
+  const [cor, setCor] = React.useState('Vermelho');
+  const [frutas, setFrutas] = React.useState('');
 
   return (
     <form>
+      <h2>Cores</h2>
+      <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
+      <h2>Frutas</h2>
+      <Radio
+        options={['Limão', 'Laranja', 'Uva']}
+        value={frutas}
+        setValue={setFrutas}
+      />
+
       <Select
         options={['Smartphone', 'Tablet']}
         value={produto}
