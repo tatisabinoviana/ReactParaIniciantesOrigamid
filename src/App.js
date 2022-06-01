c337472411f72fcdadee8dd8d00a09ada2be3237
tatisabinoviana <tatisabinoviana@gmail.com>
2022-06-01 13:48:24 -0300

    utilizando o styled-component para configuracao do css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import Produto from './Components/Produto';
+import styled from 'styled-components';
+
+const ProdutosContainer = styled.div`
+  display: flex;
+`;
+
+const Produto = styled.div`
+  flex: 1;
+`;
+
+const Titulo = styled.h1`
+  font-size: 2rem;
+  color: tomato;
+`;
+
+const Paragrafo = styled.p`
+  font-size: 1.5rem;
+  color: green;
+`;
 
 const App = () => {
   return (
     <div>
-      <Produto />
+      <ProdutosContainer>
+        <Produto>
+          <Titulo>Meu título</Titulo>
+          <Paragrafo>Meu texto é esse.</Paragrafo>
+        </Produto>
+      </ProdutosContainer>
     </div>
   );
 };
