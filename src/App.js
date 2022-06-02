fd3d52ff4c3dc6eeefe192f9f93fcb567f547a54
tatisabinoviana <tatisabinoviana@gmail.com>
2022-06-02 13:41:17 -0300

    Fazendo o css do card com o react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,65 +1,18 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const ProdutosContainer = styled.div`
-  display: flex;
-`;
-
-const Produto = styled.div`
-  flex: 1;
-`;
-
-const Titulo = styled.h1`
-  font-size: 2rem;
-  color: tomato;
-`;
-
-const Paragrafo = styled.p`
-  font-size: 1.5rem;
-  color: green;
-`;
-
-const Preco = styled.p`
-  background: hsl(${Math.random() * 360}, 100%, 50%);
-  color: white;
-`;
-const Comprar = styled.button`
-  background: ${({ ativo }) => (ativo ? '#000' : '#fff')};
-  font-size: 1rem;
-  border: 2px solid #000;
-  border-radius: 5px;
-  padding: 0.5rem;
-  color: ${({ ativo }) => (ativo ? '#fff' : '#000')};
-  cursor: pointer;
-  &:hover {
-    background: tomato;
-  }
-`;
+import Card from 'react-bootstrap/Card';
 
 const App = () => {
-  const [ativo, setAtivo] = React.useState(false);
-
-  function handleClick() {
-    setAtivo(!ativo);
-  }
   return (
-    <div>
-      <Comprar ativo={ativo} onClick={handleClick}>
-        Compre Aqui
-      </Comprar>
-      <ProdutosContainer>
-        <Produto>
-          <Titulo>Notebook</Titulo>
-          <Paragrafo>Meu texto é esse.</Paragrafo>
-          <Preco cor="#84e">R$ 2000</Preco>
-        </Produto>
-        <Produto>
-          <Titulo>Smartphone</Titulo>
-          <Paragrafo>Meu texto é esse.</Paragrafo>
-          <Preco cor="#53d956">R$ 1000</Preco>
-        </Produto>
-      </ProdutosContainer>
-    </div>
+    <Card bg="dark" text="white" className="m-5" style={{ maxWidth: '18rem' }}>
+      <Card.Header>Notebook</Card.Header>
+      <Card.Body>
+        <Card.Title>R$ 2500</Card.Title>
+        <Card.Text>
+          Texto inicia aqui, com 1Tb, 16bhsbsh akjaakak. DDDe sdkjdjierbbnnnn. E
+          termina aqui. :D
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
