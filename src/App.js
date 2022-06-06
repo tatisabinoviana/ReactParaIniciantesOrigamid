e4d3ea66cb1684167d8a0df298d81600b3f84644
Tatiana Viana <tatisabinoviana@gmail.com>
2022-06-06 19:52:25 -0300

    Animação para que os olhos da imagem saltem com o tempo e de forma gradual

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './App.css';
 import foto from './img/foto.jpg';
-import { ReactComponent as Dog } from './img/dog.svg';
+
 import DogSvg from './DogSvg';
 
 const App = () => {
+  const [olho, setOlho] = React.useState(0);
+
+  function handleClick() {
+    for(let i = 0; i < 6; i++) {
+      setTimeout(() => {
+        setOlho(i);
+      }, 100 * i);
+    }
+  }
+
   return (
     <div>
-      <p className="fundo"></p>
-      <DogSvg color="#84e" olho="5" />
+      <p className="fundo" onClick={handleClick}></p>
+      <DogSvg color="#84e" olho={olho} />
       <img src={foto} alt="Cachorro" />
     </div>
   );
