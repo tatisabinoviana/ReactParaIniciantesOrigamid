895a548f449980e19a31d7bfb5e3c04097bd3b7b
Tatiana Viana <tatisabinoviana@gmail.com>
2022-06-06 21:05:23 -0300

    Criação do router direto para a home

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import React from 'react';
-import './App.css';
-import foto from './img/foto.jpg';
-
-import DogSvg from './DogSvg';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Home'
 
 const App = () => {
-  const [olho, setOlho] = React.useState(0);
-
-  function handleClick() {
-    for(let i = 0; i < 6; i++) {
-      setTimeout(() => {
-        setOlho(i);
-      }, 100 * i);
-    }
-  }
-
   return (
-    <div>
-      <p className="fundo" onClick={handleClick}></p>
-      <DogSvg color="#84e" olho={olho} />
-      <img src={foto} alt="Cachorro" />
-    </div>
-  );
-};
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
-export default App;
+export default App
