d56e4763880b9f873f389d675c1f069c94d65c50
Tatiana Viana <tatisabinoviana@gmail.com>
2022-06-08 20:48:32 -0300

    uilizando useLocation para ter os parametros da url

--- src/Produto.js
+++ src/Produto.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
 
 const Produto = () => {
   const params = useParams()
+  const location = useLocation()
+  console.log(location)
 
   return (
     <div>
