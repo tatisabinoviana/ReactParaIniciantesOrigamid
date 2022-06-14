fc26cb6116f99aa71ff8fad9ab6cdfba0fc8da03
Tatiana Viana <tatisabinoviana@gmail.com>
2022-06-14 14:55:21 -0300

    Organizanedo o head com querySelector e setAtribute

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './Header'
 import Home from './Home'
-import Login from './Login'
-import NaoEncontrada from './NaoEncontrada'
-import Produto from './Produto'
-import ProdutoAvaliacao from './ProdutoAvaliacao'
-import ProdutoCustomizado from './ProdutoCustomizado'
-import ProdutoDescricao from './ProdutoDescricao'
+
 import Sobre from './Sobre'
 
 const App = () => {
@@ -17,13 +12,6 @@ const App = () => {
       <Routes>
         <Route path="" element={<Home />} />
         <Route path="sobre" element={<Sobre />} />
-        <Route path="login" element={<Login />} />
-        <Route path="produto/:id" element={<Produto />}>
-          <Route path="" element={<ProdutoDescricao />} />
-          <Route path="avaliacao" element={<ProdutoAvaliacao />} />
-          <Route path="customizado" element={<ProdutoCustomizado />} />
-        </Route>
-        <Route path="*" element={<NaoEncontrada />} />
       </Routes>
     </BrowserRouter>
   )
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import './Header.css'
+
 import { NavLink, useLocation } from 'react-router-dom'
 
 const Header = () => {
@@ -16,10 +16,6 @@ const Header = () => {
       |{' '}
       <NavLink to="sobre" activeStyle={{ color: 'tomato' }}>
         Sobre
-      </NavLink>{' '}
-      |{' '}
-      <NavLink to="login" activeStyle={{ color: 'tomato' }}>
-        Login
       </NavLink>
     </nav>
   )
