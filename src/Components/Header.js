adf5f50339cdfa36d41020e6ce46d876bfc9ecea
Tatiana Viana <tatisabinoviana@gmail.com>
2022-06-15 09:18:40 -0300

    iniciando desafio de quadros de Produtos e contato

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div>
+      <button>Produtos</button>
+      <button>Contato</button>
+    </div>
+  )
+}
+
+export default Header
