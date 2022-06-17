bb2d2bcc231fde8d3877e4d80b907ac01dc8dbb4
Tatiana Viana <tatisabinoviana@gmail.com>
2022-06-17 17:25:58 -0300

    Criando o Head para descrever algo na parte externa da página e implementando em contatos

--- src/Components/Contato.js
+++ src/Components/Contato.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './Contato.module.css';
 import foto from '../img/contato.jpg';
+import Head from './Head';
 
 const Contato = () => {
   return (
     <section className={styles.contato + ' animeLeft'}>
+      <Head title="Ranek | Contato" description="Entre em contato" />
       <img src={foto} alt="Máquina de escrever" />
       <div>
         <h1>Entre em contato.</h1>
