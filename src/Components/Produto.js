b520c9147dc411a1741728f3ddc2679d70f0db93
Tatiana Viana <tatisabinoviana@gmail.com>
2022-06-20 15:37:16 -0300

    Adicao do comopente Produto, distribuicao dos produtos de forma individual e modificacao das classes css

---
+++ src/Components/Produto.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Produto.module.css';
+
+const Produto = () => {
+  return (
+    <div className={style.produto}>
+      <h1>Produto</h1>
+    </div>
+  );
+};
+
+export default Produto;
