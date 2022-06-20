934dee140bb3c987ec4f3e9afea7f2e36303fdb4
Tatiana Viana <tatisabinoviana@gmail.com>
2022-06-20 17:22:35 -0300

    incrementando o componente produto para aparecer as fotos relacionadas, titulo, valor e descricao do produto

--- src/Components/Produto.js
+++ src/Components/Produto.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import style from './Produto.module.css';
+import { useParams } from 'react-router-dom';
 
 const Produto = () => {
+  const [produto, setProduto] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  const { id } = useParams();
+
+  React.useEffect(() => {
+    async function fetchProduto(url) {
+      setLoading(true);
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setProduto(json);
+      } catch (erro) {
+        setError('Um erro ocorreu');
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchProduto(`https://ranekapi.origamid.dev/json/api/produto/${id}`);
+  }, [id]);
+
+  if (loading) return <div>Carregando...</div>;
+  if (error) return <p>{error}</p>;
+  if (produto === null) return null;
+
   return (
-    <div className={style.produto}>
-      <h1>Produto</h1>
-    </div>
+    <section className={style.produto + ' animeLeft'}>
+      {produto.fotos.map(foto => (
+        <img key={foto.src} src={foto.src} alt={foto.titulo} />
+      ))}
+      <div>
+        <h1>{produto.nome}</h1>
+        <span className={style.preco}>R$ {produto.preco}</span>
+        <p className={style.descricao}>{produto.descricao}</p>
+      </div>
+    </section>
   );
 };
 
